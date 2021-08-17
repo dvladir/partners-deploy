776c41cb2cf307163978b1108dbbc716c1553576
dvladir <dragonvladir@gmail.com>
2021-08-18 01:50:39 +0300

    Script fix

--- partners-deploy/scripts/recreate.sh
+++ partners-deploy/scripts/recreate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "RECREATE $1"
-docker-compose up --force-recreate --no-deps -d $1
+ARC=$1
+IMG=$2
+SRV=$3
+echo "RECREATE $ARC $IMG $SRV"
+docker import $ARC $IMG
+docker image prune -f
+docker-compose up --force-recreate --no-deps -d $SRV
